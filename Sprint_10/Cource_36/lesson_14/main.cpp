80ef3262d061d44a2b8cb60f7402515ed576e6ea
Stanislav <vasilevstas.forgit@bk.ru>
2025-05-16 01:58:03 +0300

    del

--- Sprint_10/Cource_36/lesson_14/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "json_reader.h"
-#include "request_handler.h"
-
-int main() {
-    transport::Catalogue catalogue;
-    JsonReader requests(std::cin);
-    requests.FillCatalogue(catalogue);
-    RequestHandler(requests, catalogue);
-}
